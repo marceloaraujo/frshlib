b7faedbc0a927c1cfc2cead9709833539cacba38
Marcelo Araujo <marcelo.araujo@loupen.com.br>
2018-02-27 09:22:51 -0300

    Add some methods

--- frshlib.js
+++ frshlib.js
@@ -3,7 +3,7 @@
  * A ideia é criar essa lib para não precisar reescrever código a cada
  * app criado para o Freshdesk 😎
  * Made by Marcelo Araujo
- * Twitter: @m_araujo01
+ * Twitter: https://twitter.com/m_araujo01
  * 
 */
 
@@ -12,6 +12,10 @@ var frshlib = (function(){
     return {
         freshDeskUrl: "",
         freshDeskKey: "",
+        /**
+         * Init client with Freshdesk URL and API key
+         * @author Marcelo Araujo
+         */
         init: function(freshDeskUrl, freshDeskKey){
             this.freshDeskUrl = freshDeskUrl;
             this.freshDeskKey = freshDeskKey;
@@ -27,41 +31,106 @@ var frshlib = (function(){
             return true;
         },
 
+        /**
+         * Create a ticket in Freshdesk
+         * @author Marcelo Araujo
+         */
         createTicket: function(ticketData) {
             var _this = this;
             var url = _this.freshDeskUrl + "/api/v2/tickets";
             return _this._callPromise(url, "POST", ticketData);
         },
 
+        /**
+         * View a ticket by id
+         * @author Marcelo Araujo
+         */
         viewTicket: function(idTicket) {
             var _this = this;
             var url = _this.freshDeskUrl + "/api/v2/tickets/" + idTicket;
             return _this._callPromise(url, "GET");
         },
 
+        /**
+         * View all tickets
+         * @author Marcelo Araujo
+         */
         viewAllTickets: function() {
             var _this = this;
             var url = _this.freshDeskUrl + "/api/v2/tickets";
             return _this._callPromise(url, "GET");
         },
 
+        /**
+         * Update a ticket by id
+         * @author Marcelo Araujo
+         */
         updateTicket: function(idTicket, ticketData) {
             var _this = this;
             var url = _this.freshDeskUrl + "/api/v2/tickets/" + idTicket;
             return _this._callPromise(url, "PUT", ticketData);
         },
 
+        /**
+         * Delete a ticket by id
+         * @author Marcelo Araujo
+         */
         deleteTicket: function(idTicket) {
             var _this = this;
             var url = _this.freshDeskUrl + "/api/v2/tickets/" + idTicket;
             return _this._callPromise(url, "DELETE");
         },
 
+        /**
+         * Create a reply in ticket
+         * @author Marcelo Araujo
+         */
+        createReply: function(idTicket, replyData) {
+            var _this = this;
+            var url = _this.freshDeskUrl + "/api/v2/tickets/" + idTicket + "/reply";
+            return _this._callPromise(url, "POST", replyData);
+        },
+
+        /**
+         * Create a note in ticket
+         * @author Marcelo Araujo
+         */
+        createNote: function(idTicket, noteData) {
+            var _this = this;
+            var url = _this.freshDeskUrl + "/api/v2/tickets/" + idTicket + "/notes";
+            return _this._callPromise(url, "POST", noteData);
+        },
+
+        /**
+         * Update a note by id
+         * @author Marcelo Araujo
+         */
+        updateConversation: function(idNote, noteData) {
+            var _this = this;
+            var url = _this.freshDeskUrl + "/api/v2/conversations/" + idNote;
+            return _this._callPromise(url, "PUT", noteData);
+        },
+
+        /**
+         * Delete a conversation by id
+         * @author Marcelo Araujo
+         */
+        deleteConversation: function(idConversation) {
+            var _this = this;
+            var url = _this.freshDeskUrl + "/api/v2/conversations/" + idConversation;
+            return _this._callPromise(url, "DELETE");
+        },
+
+        /**
+         * Generic function to call API by URL.
+         * This function is used in all functions above
+         * @author Marcelo Araujo
+         */
         _callPromise: function(url, method, data) {
             var _this = this;
             return new Promise(function(resolve, reject) {
                 if(!_this.validateFreshdeskInfos()) {
-                    reject("Por favor, use a função init para enviar a URL e API Key do Freshdesk!");
+                    reject("Please, use the init function to send the URL and API Key of Freshdesk!");
                 } else {
                     var dataPost = JSON.stringify(data);
                     jQuery.ajax({
